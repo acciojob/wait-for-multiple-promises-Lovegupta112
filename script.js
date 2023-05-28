a979ed24f505db6fc8b38afe30745fbd57d9d3be
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-05-28 13:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,32 +17,27 @@ let prom3=new Promise((resolve,reject)=>{
           },2500);
 })
 
-prom1.then(()=>{
+let allResolved=Promise.all([prom1,prom2,prom3])
+
+allResolved.then(()=>{
     let td1=document.createElement('td');
     td1.innerText="Promise 1";
     let td2=document.createElement('td');
     td2.innerText="1500s";
-	tbody.children[0].innerHTML="";
-    tbody.children[0].append(td1);
-    tbody.children[0].append(td2);
-})
-prom2.then(()=>{
-    let td1=document.createElement('td');
+    let td2=document.createElement('td');
     td1.innerText="Promise 2";
-    let td2=document.createElement('td');
+    let td3=document.createElement('td');
     td2.innerText="2000s";
-	tbody.children[0].innerHTML="";
-    tbody.children[0].append(td1);
-    tbody.children[0].append(td2);
-})
-prom3.then(()=>{
-    let td1=document.createElement('td');
+    let td4=document.createElement('td');
     td1.innerText="Promise 3";
-    let td2=document.createElement('td');
-    td2.innerText="3000s";
+    let td5=document.createElement('td');
+    td2.innerText="1500s";
 	tbody.children[0].innerHTML="";
     tbody.children[0].append(td1);
     tbody.children[0].append(td2);
+    tbody.children[0].append(td2);
+    tbody.children[0].append(td3);
+    tbody.children[0].append(td4);
+    tbody.children[0].append(td5);
 })
 
-Promise.all([prom1,prom2,prom3])
