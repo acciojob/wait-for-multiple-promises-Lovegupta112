22314f9cd6dc4a44f20fbc5059282ffb980e05d0
lovegupta112 <loveguptaofficials@gmail.com>
2023-06-05 21:36:21 +0530

    updated code

--- script.js
+++ script.js
@@ -39,18 +39,19 @@ let prom3=new Promise((resolve,reject)=>{
     let randomTime=Math.floor(Math.random()*(3000-1000+1))+1000;
           setTimeout(()=>{
          resolve();
+
          let end3=new Date().getTime();
         //  addPromise('Promise 3',(end3-start3)/1000)
          addPromise('Promise 3',start3,end3);
           },randomTime);
+
 })
 
 let allResolved=Promise.all([prom1,prom2,prom3])
 
 allResolved.then(()=>{
-   addToUI(promisesList);
+    addToUI(promisesList);
 })
-
 
 function addToUI(promisesList){
     tbody.innerHTML="";
@@ -65,28 +66,3 @@ function addToUI(promisesList){
     row.innerHTML=`<td>Total</td><td>${(maxTime-minTime)/1000}</td>`
     tbody.append(row);
 }
-// allResolved.then(()=>{
-//     let td1=document.createElement('td');
-//     td1.innerText="Promise 1";
-//     let td2=document.createElement('td');
-//     td2.innerText="1500s";
-//     let td3=document.createElement('td');
-//     td3.innerText="Promise 2";
-//     let td4=document.createElement('td');
-//     td4.innerText="2000s";
-//     let td5=document.createElement('td');
-//     td5.innerText="Promise 3";
-//     let td6=document.createElement('td');
-//     td6.innerText="1500s";
-//     let tr1=document.createElement('tr');
-//     let tr2=document.createElement('tr');
-//     let tr3=document.createElement('tr');
-// 	tbody.children[0].innerHTML="";
-//     tbody.children[0].append(td1);
-//     tbody.children[0].append(td2);
-//     tbody.children[0].append(td3);
-//     tbody.children[0].append(td4);
-//     tbody.children[0].append(td5);
-//     tbody.children[0].append(td6);
-// })
-
